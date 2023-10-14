9a2206bf08f9ae6c2e8521867610c66624a0bcb6
Igor Rocha <igor_roc@hotmail.com.br>
2023-10-14 12:03:31 -0300

    fix: Search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,31 @@
+"use client"
+
 import Image from "next/image"
 import styles from "./page.module.css"
 import Logo from "/public/icon/favicon.png"
+import { useRouter } from "next/navigation"
 
 export default function Home() {
+	const router = useRouter()
+
+	function handleSubmit(ev: React.FormEvent<HTMLFormElement>) {
+		ev.preventDefault()
+		const formData = new FormData(ev.currentTarget)
+		const id = formData.get("id")
+		console.log(id)
+
+		router.push(`/${id}`)
+	}
 	return (
 		<main className={styles.main}>
 			<Image src={Logo} alt="Logo do MeusPetz" />
 			<h1>MeusPetz</h1>
 			<p>Encontrou um pet perdido?</p>
 			<p>Coloque o nome dele aqui em baixo e nós avisaremos ao seu dono</p>
-			<input type="text" />
+			<form onSubmit={handleSubmit} className={styles.form}>
+				<input type="text" name="id" />
+				<button>Buscar</button>
+			</form>
 		</main>
 	)
 }
